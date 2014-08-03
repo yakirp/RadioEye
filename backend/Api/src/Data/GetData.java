1702ddb59551dddc4017dfe06ec557746b8a02ad
yakirp <yakirperlin@gmail.com>
2014-08-03 07:44:03 +0300

    Organized import

--- backend/Api/src/Data/GetData.java
+++ backend/Api/src/Data/GetData.java
@@ -4,10 +4,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
-
-import org.codehaus.jettison.json.JSONException;
-import org.json.JSONObject;
-
 import com.pubnub.api.Callback;
 import com.pubnub.api.Pubnub;
 import com.pubnub.api.PubnubError;
--- backend/Api/src/Data/SetData.java
+++ backend/Api/src/Data/SetData.java
@@ -4,16 +4,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import org.codehaus.jettison.json.JSONException;
-
-import org.json.JSONArray;
- 
 import org.json.JSONObject;
-
 import com.pubnub.api.Callback;
 import com.pubnub.api.Pubnub;
 import com.pubnub.api.PubnubError;
